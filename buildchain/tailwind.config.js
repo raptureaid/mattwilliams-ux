571f8ddc5ff164760c0f65f5f4798531ea04d8f1
raptureaid <5720326+raptureaid@users.noreply.github.com>
2021-02-25 09:54:01 -0500

    Env based pruge CSS; Theme colors and font sizes

--- buildchain/tailwind.config.js
+++ buildchain/tailwind.config.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
+// Environment
+const environment = process.env.ENVIRONMENT || 'development';
+
 // tailwind.config.js
 const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: {
+    enabled: environment === 'production',
     content: [
       '../cms/templates/**/*.{twig,html}',
     ],
@@ -12,18 +18,19 @@ module.exports = {
       'components',
       'utilities',
     ],
-  },
-  mode: 'layers',
-  options: {
-    whitelist: [
-      '../src/css/components/**/*.{css}',
-    ],
+    mode: 'layers',
+    options: {
+      whitelist: [
+        '../src/css/components/**/*.{css}',
+      ],
+    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
+      blue: colors.lightBlue,
       gray: colors.blueGray,
       teal: colors.teal,
     },
@@ -31,7 +38,15 @@ module.exports = {
       title: ['Righteous', ...defaultTheme.fontFamily.sans],
       title2: ['Quicksand', ...defaultTheme.fontFamily.sans],
     },
-    extend: {},
+    extend: {
+      fontSize: {
+        '6xl': '3.5rem',
+        '7xl': '3.75rem',
+        '8xl': '4.5rem',
+        '9xl': '6rem',
+        '10xl': '8rem', 
+      }
+    },
   },
   variants: {},
   plugins: [],
